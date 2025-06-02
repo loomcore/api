c206217d2c5aa00db3c0311a23aaa54c3ccf4243
Tim Hardy <tim.hardy@omegacreations.com>
2025-06-02 12:49:38 -0500

    Updates

--- src/config/base-api-config.ts
+++ src/config/base-api-config.ts
@@ -3,9 +3,18 @@ import {IBaseApiConfig} from '../models/index.js';
 export let config: IBaseApiConfig;
 let isConfigSet = false;
 
+// Utility function to pick only IBaseApiConfig properties. We ignore any extended properties that are not in IBaseApiConfig.
+function copyOnlyBaseApiConfigProperties<T extends IBaseApiConfig>(obj: T): IBaseApiConfig {
+  const baseConfig: IBaseApiConfig = {} as IBaseApiConfig;
+  (Object.keys(obj) as (keyof IBaseApiConfig)[]).forEach((key) => {
+    (baseConfig as any)[key] = obj[key];
+  });
+  return baseConfig;
+}
+
 export function setBaseApiConfig(baseApiConfig: IBaseApiConfig) {
   if (!isConfigSet) {
-    config = baseApiConfig;
+    config = copyOnlyBaseApiConfigProperties(baseApiConfig);
     isConfigSet = true;
   } else if (config.env !== 'test') {
     console.warn('BaseApiConfig data has already been set. Ignoring subsequent calls to setBaseApiConfig.');
