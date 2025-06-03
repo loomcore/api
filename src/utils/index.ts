4b10f5ed17ac12c2da732e366060a9c52a950deb
Tim Hardy <tim.hardy@omegacreations.com>
2025-06-03 18:40:01 -0500

    Updates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './address.utils.js';
 export * from './api.utils.js';
 export * from './conversion.utils.js';
 export * from './db.utils.js';
+export * from './express.utils.js';
 export * from './password.utils.js';
 export * from './string.utils.js';
