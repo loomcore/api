540bf4772b81ef4d2b08e4cd2f84340dceaaa199
Phillip Jacobs <phillip.c.jacobs@gmail.com>
2025-12-12 16:17:32 -0600

    Updates

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,7 @@ export * from './api.controller.js';
 export * from './auth.controller.js';
 export * from './organizations.controller.js';
 export * from './users.controller.js';
+export * from './user-roles.controller.js';
+export * from './roles.controller.js';
+export * from './authorizations.controller.js';
+export * from './features.controller.js';
