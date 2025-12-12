99dbedc272abf1f0d172584756c71c5a2aecd762
Phillip Jacobs <phillip.c.jacobs@gmail.com>
2025-12-12 13:51:23 -0600

    Updates

--- src/databases/postgres/migrations/index.ts
+++ src/databases/postgres/migrations/index.ts
@@ -1,3 +1,2 @@
 export * from './migration.interface.js';
-export * from './setup-for-multitenant.migration.js';
-export * from './setup-for-auth.migration.js';
+export * from './database-builder.js';
