5df62040c4aa36511ce4b9a139436a26d80eff74
Tim Hardy <tim.hardy@omegacreations.com>
2025-12-30 19:35:46 -0600

    Updates

--- src/models/refresh-token.model.ts
+++ src/models/refresh-token.model.ts
@@ -1,24 +1,25 @@
 import { IEntity } from "@loomcore/common/models";
+import type { AppId } from "@loomcore/common/types";
 import { entityUtils } from "@loomcore/common/utils";
-import { TypeboxIsoDate } from "@loomcore/common/validation";
+import { TypeboxIsoDate, getIdSchema } from "@loomcore/common/validation";
 import { Type } from "@sinclair/typebox";
 
 export interface IRefreshToken extends IEntity {
 	token: string;
 	deviceId: string;
-	userId: string;
+	userId: AppId;
 	expiresOn: number;
 	created: Date;
-	createdBy: string;
+	createdBy: AppId;
 };
 
 export const refreshTokenSchema = Type.Object({
     token: Type.String({ minLength: 1 }),
     deviceId: Type.String({ minLength: 1 }),
-    userId: Type.String({ minLength: 1 }),
+    userId: getIdSchema(),
     expiresOn: Type.Number(),
     created: TypeboxIsoDate({ title: 'Created Date' }),
-    createdBy: Type.String({ minLength: 1 })
+    createdBy: getIdSchema()
   });
   
 export const refreshTokenModelSpec = entityUtils.getModelSpec(refreshTokenSchema);
