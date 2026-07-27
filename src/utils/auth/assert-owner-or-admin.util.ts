012313d4c7fd047a05fa85e506307d730ea788db
Phillip Jacobs <phillip.c.jacobs@gmail.com>
2026-07-27 14:48:43 -0500

    just pass id to assertOwnerOrAdmin

--- src/utils/auth/assert-owner-or-admin.util.ts
+++ src/utils/auth/assert-owner-or-admin.util.ts
@@ -1,15 +1,16 @@
 import type { IAuditable, IUserContext } from "@loomcore/common/models";
 import { UnauthorizedError } from "../../errors/index.js";
 import { isAdmin } from "./is-admin.util.js";
+import { AppIdType } from "@loomcore/common/types";
 
 export function assertOwnerOrAdmin(
 	userContext: IUserContext,
-	entity: IAuditable,
+	createdBy: AppIdType,
 ): void {
 	if (isAdmin(userContext)) {
 		return;
 	}
-	if (userContext.user._id !== entity._createdBy) {
+	if (userContext.user._id !== createdBy) {
 		throw new UnauthorizedError();
 	}
 }
